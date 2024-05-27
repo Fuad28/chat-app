88b178a2269134a1efd01e9dc5789215a25c2e92
Fuad <fuadadio75@gmail.com>
2024-05-27 12:31:21 +0100

    refactory: removed auto_now from updated_at fields and moved logic to save method. Removed is_sent from Message

--- api/models/conversation.py
+++ api/models/conversation.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 import uuid
 
@@ -28,14 +29,10 @@ class Conversation(models.Model):
 		related_name= "conversations"
 	)
 
-	created_at= models.DateTimeField(auto_now= True)
-	updated_at= models.DateTimeField(auto_now_add= True)
+	created_at= models.DateTimeField(auto_now_add= True)
+	updated_at= models.DateTimeField(**null_blank)
 	is_private= models.BooleanField(default= False)
 
-
-	def __str__(self):
-		return f"{self.id}-{self.name}"
-	
 	def to_dict(self):
 		return {
 			"id": str(self.id),
@@ -43,13 +40,21 @@ class Conversation(models.Model):
 			"is_private": self.is_private,
 			"created_by": self.created_by.get_full_name()
 		}
+
+	def __str__(self):
+		return f"{self.id}-{self.name}"
+	
+	def save(self, *args, **kwargs):
+		if self.pk:  
+			self.updated_at = timezone.now()
+		super().save(*args, **kwargs)
 		
 class ConversationMembers(models.Model):
 	""" Holds members of a conversation record. """
 	
 	user= models.ForeignKey(User, on_delete= models.CASCADE)
 	conversation= models.ForeignKey(Conversation, on_delete= models.CASCADE)
-	joined_at= models.DateTimeField(auto_now= True)
+	joined_at= models.DateTimeField(auto_now_add= True)
 	is_admin= models.BooleanField(default= False)
 
 
@@ -78,17 +83,21 @@ class Message(models.Model):
 		related_name= "seen_messages"
 		)
 	
-	sent_at= models.DateTimeField(auto_now= True)
-	updated_at= models.DateTimeField(auto_now_add= True)
+	sent_at= models.DateTimeField(auto_now_add= True)
+	updated_at= models.DateTimeField(**null_blank)
 	media_url= models.URLField(**null_blank)
 	text= models.TextField(**null_blank)
 	message_type= models.CharField(max_length= 50, choices= MessageTypeEnum.choices)
-	is_sent = models.BooleanField(default= False)
 
 	objects= CachedMessageQuerySet.as_manager()
 
 	def __str__(self):
 		return f"{self.id}"
+	
+	def save(self, *args, **kwargs):
+		if self.pk:  
+			self.updated_at = timezone.now()
+		super().save(*args, **kwargs)
 	
 class MessageViewers(models.Model):
 	""" Holds users that have seen a message. """
